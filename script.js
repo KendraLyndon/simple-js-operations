39f91208b708735e31b69c3eea0ac4de09ffb677
Kendra Lyndon <kendralyndon@gmail.com>
2016-05-27 11:26:39 -0600

    add solutions

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 module.exports = {
-  stringToArray: function(){
-
+  stringToArray: function(str){
+    return str.split(' ');
   },
-  swapInPlace: function(){
-
+  swapInPlace: function(arr, a, b){
+    var temp = arr[a];
+    arr.splice(arr[a],1,arr[b]);
+    arr.splice(b,1,temp);
+    return arr;
   },
-  arrayToString: function(){
-
+  arrayToString: function(arr, char){
+    return char ? arr.join(char) : arr.join(' ');
   },
-  keys: function(){
-
+  keys: function(obj){
+    return Object.keys(obj);
   }
 };
